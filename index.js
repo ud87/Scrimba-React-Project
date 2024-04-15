5bebb7146642d082dfc2904a1816c0473992a3ee
Udaya <udayarai@hotmail.co.uk>
2024-04-15 21:58:21 +0100

    JSX added to index.js and rendered to page

--- index.js
+++ index.js
@@ -13,5 +13,21 @@ Hints:
   just like in HTML. In the slide, I have it set to 40px
  */
 
+import React from "react";
+import ReactDOM from "react-dom";
 
+const page = (
+  <div>
+    <img src="./react-logo.png" width="40" alt="React logo"></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
 
+ReactDOM.render(page, document.getElementById("root"));
